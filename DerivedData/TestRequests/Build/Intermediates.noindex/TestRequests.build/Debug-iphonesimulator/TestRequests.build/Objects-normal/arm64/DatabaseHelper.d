becac3cfc930d5a0b477d463300013ebd505ad63
Nazar Pallaev <npallaev@it-one.ru>
2022-03-29 15:23:55 +0300

    DerivedData removed

--- DerivedData/TestRequests/Build/Intermediates.noindex/TestRequests.build/Debug-iphonesimulator/TestRequests.build/Objects-normal/arm64/DatabaseHelper.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/nazar/Documents/TestRequests/TestRequests/Helpers/DatabaseHelper.c \
-  /Users/nazar/Documents/TestRequests/TestRequests/Helpers/DatabaseHelper.h
--- DerivedData/TestRequests/Build/Intermediates.noindex/TestRequests.build/Debug-iphonesimulator/TestRequests.build/Objects-normal/arm64/TestFile.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/nazar/Documents/TestRequests/TestRequests/TestFile.c \
-  /Users/nazar/Documents/TestRequests/TestRequests/TestFile.h
