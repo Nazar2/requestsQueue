3c6223a2a4a8fbb5dbe2fa1b79e87ebf2383d450
Nazar Pallaev <npallaev@it-one.ru>
2022-03-29 15:20:44 +0300

    Initial Commit

---
+++ DerivedData/TestRequests/Build/Intermediates.noindex/TestRequests.build/Debug-iphonesimulator/TestRequests.build/Objects-normal/arm64/DatabaseHelper.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/nazar/Documents/TestRequests/TestRequests/Helpers/DatabaseHelper.c \
+  /Users/nazar/Documents/TestRequests/TestRequests/Helpers/DatabaseHelper.h
---
+++ DerivedData/TestRequests/Build/Intermediates.noindex/TestRequests.build/Debug-iphonesimulator/TestRequests.build/Objects-normal/arm64/TestFile.d
@@ -0,0 +1,2 @@
+dependencies: /Users/nazar/Documents/TestRequests/TestRequests/TestFile.c \
+  /Users/nazar/Documents/TestRequests/TestRequests/TestFile.h
